9f2031db57a9bbdab2ce2d6359754fee57a030e4
Sega NDIAYE <ndiaye.sega.pro@gmail.com>
2025-08-25 19:18:07 +0200

    Main

---
+++ wqp/main.py
@@ -0,0 +1,7 @@
+"""
+Main
+"""
+
+if __name__ == '__main__':
+    from wqp.workflow import model_training_workflow
+    model_training_workflow(data_path='http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv')
