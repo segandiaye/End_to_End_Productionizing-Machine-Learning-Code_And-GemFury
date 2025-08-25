e1aa17d4de5b5e1386faff877c0e834a2833879b
Sega NDIAYE <ndiaye.sega.pro@gmail.com>
2025-08-25 20:17:46 +0200

    Fix errors

--- wqp/ml.py
+++ wqp/ml.py
@@ -9,6 +9,6 @@ from sklearn.pipeline import Pipeline
 def build_wine_predictor_model() -> Pipeline:
 
     model: Pipeline = Pipeline(steps=[
-        ('regression', ElasticNet(alpha=1.0, l1_ratio=0.5, fit_intercept=True, normalize=False))
+        ('regression', ElasticNet(alpha=1.0, l1_ratio=0.5, fit_intercept=True))
     ])
     return model
