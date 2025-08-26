a0aff1138b14890d3f81b35e1ec99f08c7852cab
Sega NDIAYE <ndiaye.sega.pro@gmail.com>
2025-08-26 10:52:29 +0200

    Command line tool

--- wqp/cli.py
+++ wqp/cli.py
@@ -6,3 +6,14 @@ from wqp import __version__
 @click.version_option(__version__)
 def wqp():
     pass
+
+
+@wqp.group(name='jobs', help='Set of commands to launch training or evaluation jobs')
+def jobs():
+    pass
+    
+@jobs.command(name='train', help='Executes the wine predictor training workflow')
+@click.option('--data-path', '-d', help='The path were training data can be found')
+def train(data_path: str):
+    import wqp.workflow as wf
+    wf.model_training_workflow(data_path=data_path)
